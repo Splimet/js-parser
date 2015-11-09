c364c3da16f90d63e6250b07c12e96f073bd46ee
Splimet <splimet@gmail.com>
2015-11-09 22:45:08 +0300

    Bugfix checkbox and added form adding new parameter

--- js/parser.js
+++ js/parser.js
@@ -1,3 +1,8 @@
+var 
+		TYPE_STRING = 'System.String',
+		TYPE_INT = 'System.Int32',
+		TYPE_BOOL = 'System.Boolean';
+		
 $(document).ready(function () {
     $.ajax({
         type: "GET",
@@ -9,8 +14,8 @@ $(document).ready(function () {
 
 function xmlParser(xml) {
     $(xml).find("Parameter").each(function () {				
-				$("#parameters").append(createRow({ value: $(this).find("Name").text(), type: 'System.String'}, 
-																					{ value: $(this).find("Description").text(), type: 'System.String'},																				
+				$("#parameters").append(createRow({ value: $(this).find("Name").text(), type: TYPE_STRING}, 
+																					{ value: $(this).find("Description").text(), type: TYPE_STRING},																				
 																					{ value: $(this).find("Value").text(), type: $(this).find("Type").text()}));
     });
 
@@ -27,14 +32,14 @@ function createRow(name, description, parameter) {
 
 function createCell( field ){
 	switch ( field.type ) {
-		case 'System.String':
-			return '<input type="text" onchange="changeValueInField(this)" value=' + field.value + '>';
-		case 'System.Int32':
-			return '<input type="number" oninput="checkFieldForNumber(this); changeValueInField(this)" value=' + field.value + '>';
-		case 'System.Boolean':
-			if ( value === 'True' )
-				return '<input type="checkbox" onchange="changeValueInField(this)" checked="checked">';
-			else
-				return '<input type="checkbox" onchange="changeValueInField(this)" >';
+		case TYPE_STRING:
+			return '<input type="text" onchange="" value=' + field.value + '>';
+		case TYPE_INT:
+			return '<input type="number" oninput="" value=' + field.value + '>';
+		case TYPE_BOOL:
+			var checked = '';
+			if ( field.value === 'True' )
+				checked = 'checked';
+			return '<input type="checkbox" onchange="" ' + checked + '>';
 	}
 }
